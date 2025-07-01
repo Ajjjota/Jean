d4727608856bad4778ea3eede8c655271ac90270
Ajjjota <bernaljean2004@gmail.com>
2025-07-01 14:55:32 -0400

    Add files via upload
    arreglado

--- Suma.py
+++ Suma.py
@@ -1,10 +1,13 @@
 def suma(a, b):
     return a + b
 
+while True:
+ try:
 
-l1 = int(input("Ingrese el primer número: "))
-l2 = int(input("Ingrese el segundo número: "))
-
-
+    l1 = int(input("Ingrese el primer número: "))
+    l2 = int(input("Ingrese el segundo número: "))
+ except:
+    print("Ingrese numero enteros")  
+    break 
 resultado = suma(l1,l2)
 print(resultado)
